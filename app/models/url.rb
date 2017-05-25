90e5e1a4961bc8d799b31828838cb1ffa56716bc
Suhnylla Kler <suhnylla_kler@yahoo.com>
2017-05-25 19:18:28 +0800

    models shorten_method

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,15 @@
 class Url < ActiveRecord::Base
+	validates :long_url, presence: true
+	validates :long_url, format: { with: /\A(https|http):\/\// }
+	before_save :shorten
+	
+	# This is Sinatra! Remember to create a migration!
+	def shorten
+		
+		range = [*'0'..'9',*'A'..'Z',*'a'..'z']
+
+		@short_url = (0...6).map{ range.sample }.join
+		self.short_url = @short_url
+	
+	end
 end
